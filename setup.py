c9ef0ecaf20c17196e2870145cc031420d82f2a4
digidigital <bjoern@seipelspace.de>
2025-09-22 23:17:36 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="iconipy",
-    version="0.4.0",
+    version="0.5.1",
     author="Björn Seipel",
     author_email="support@digidigital.de",
     description="Create icons for user interfaces directly from your Python code.",
